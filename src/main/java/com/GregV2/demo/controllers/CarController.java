1cf3cdfe40f2f14525bc28cdd8c3db75dd41f3da
Greg <maza.gregory46@gmail.com>
2021-10-28 23:01:03 +0200

    feat: Controller CRUD

--- src/main/java/com/GregV2/demo/controllers/CarController.java
+++ src/main/java/com/GregV2/demo/controllers/CarController.java
@@ -3,27 +3,44 @@ package com.GregV2.demo.controllers;
 import com.GregV2.demo.dao.CarDao;
 import com.GregV2.demo.model.Car;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@RestController //Transforme l'objet JAVA en JSON grace à la dépendence JACKSON
 public class CarController {
 
-    @Autowired
+    @Autowired // on lui indiue qu'on a accès à une couche de données (Le controller doit instancier)
     private CarDao carDao;
 
     @GetMapping(value = "/Cars")
-    public List<Car> getCars(){
+    public List<Car> listeCars(){
+
         return carDao.findAll();
     }
 
     @GetMapping(value="/Cars/{id}")
-    public String CarId(@PathVariable int id){
+    public Car AfficherCar(@PathVariable int id){
+        //Car car = new Car(id,"Peugeot","308 Tech Edition","Grise",27000);
 
-        return carDao.getById();
+        return carDao.getById(id);
+    }
+
+    @GetMapping(value="/Cars/delete/{id}")
+    public Boolean SupprimerCar(@PathVariable int id){
+        return carDao.delete(id);
+    }
+
+    @PostMapping(value="/Cars/add")
+    public Car AjouterCar(@RequestBody Car car){ //@RequestBody
+        carDao.save(car);
+        return car;
+    }
+
+    @PostMapping(value="/Cars/update/{id}")
+    public Car ModifierCar(@RequestBody Car updateCar, @PathVariable int id){
+        carDao.update(updateCar,id);
+        return updateCar;
     }
 
 }
